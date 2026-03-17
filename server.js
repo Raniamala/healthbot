586478f100fd67d6909ad9e84827ec2b6e52a6d0
raniamala <raniamala2@gmail.com>
2026-03-17 21:49:28 +0530

    cors

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 const chatRoute = require("./routes/chat");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*"
+  })
+);
 app.use(express.json());
 
 app.use("/api/chat", chatRoute);
